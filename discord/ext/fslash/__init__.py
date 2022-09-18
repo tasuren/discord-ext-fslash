8999b54d040a891f818cd127a43cf2eb49018668
tasuren <tasuren_0618@yahoo.co.jp>
2022-09-18 14:51:43 +0900

    update: バージョンを上げた。

--- discord/ext/fslash/__init__.py
+++ discord/ext/fslash/__init__.py
@@ -22,7 +22,7 @@ __all__ = (
     "extend_force_slash", "is_fslash", "Context",
     "groups", "exceptions", "adjustment_command_name"
 )
-__version__ = "0.1.25"
+__version__ = "0.1.26"
 __author__ = "tasuren"
 
 
